d1fce792846a8538cb09f1ee952cf3eb4fdec668
Vitor Nascimento <vnasm7140@gmail.com>
2026-07-02 17:16:17 -0300

    feat: Mudando objetivo do site para uma lista de tarefas.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,82 @@
-function meuSite () {
-  const formulario = document.querySelector('.forms');
-  const resultado = document.querySelector('.comanda');
-  const resetar = document.querySelector('.reset');
+function myPage () {
+  const form = document.querySelector('.forms');
+  const result = document.querySelector('.comand');
+  const taskUser = document.querySelector('.taskPen');
+  const exeTask = document.querySelector('.taskExe');
   
+  const tasksPen = [];
+  const tasksExe = [];
+  const tasksConc = [];
+  const tasksInc = [];
+  
+  
+  function moveTask(div) {
+    exeTask.appendChild(div);
+  }
+  
+  
+  function waitEvent(event) {
+    event.preventDefault();
+    const tarefa = document.getElementById("tarefas-usuario").value;    
     
-  function resetarFormulario(limparForms) {
-    limparForms.preventDefault();  
-    formulario.reset();
+    function pushTaskExe() {
+      tasksExe.push({
+        tarefaUsuario: tarefa,
+      })
   }
 
-    const forms = [];
+    if(tarefa === ''){
+      alert("Você não adicionou nenhuma tarefa.");
+      return;
+    }
     
-    function aguardarEvento(evento) {
-    evento.preventDefault();
-    
-    const nome = document.getElementById("nome-usuario");
-    const data = document.getElementById("dataNascimento");
-    const genero = document.getElementById("genero-usuario");
-    const num = document.getElementById("numero-usuario");
-    
-
-    forms.push({
-      nomeUsuario: nome.value,
-      dataNascimento: data.value,    
-      generoUsuario: genero.value,
-      numero: num.value,
+    tasksPen.push({
+      tarefaUsuario: tarefa,
     });
-    console.log(forms);
 
+
+    const divTasks = document.createElement('div');
+    const p = document.createElement('p');
+    
+    divTasks.classList.add('taskAddUser');
+    p.classList.add('textTask');
+    
+    
+    divTasks.appendChild(p);
+    createBtns(divTasks);
+    pushTaskExe;
+    
+    taskUser.appendChild(divTasks);
+    
+    console.log(tasksPen);
+    console.log(tasksExe);
+    p.textContent += tarefa;
+    
   }
-    resultado.addEventListener('click', aguardarEvento);
-    resetar.addEventListener('click', resetarFormulario);
+  
+  result.addEventListener('click', waitEvent);
+
+    function createBtns(div){
+      const btnSetTask = document.createElement('button');
+    const btnDel = document.createElement('button');
+    
+    btnSetTask.classList.add('btnSetTask');
+    btnDel.classList.add('DelTask');
+
+    btnSetTask.textContent = 'iniciar'
+    btnDel.textContent = 'X'
+    btnDel.title = 'Deletar Tarefa'
+
+    div.appendChild(btnSetTask);
+    div.appendChild(btnDel);
+    
+    btnSetTask.addEventListener('click', () => {
+      moveTask(div);
+    });
     
   }
 
 
-meuSite();
+}
+
+myPage();
