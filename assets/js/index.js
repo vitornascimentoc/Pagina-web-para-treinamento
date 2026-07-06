40371369be9bfc93a4c33d205d9fda24babf5567
Vitor Nascimento <vnasm7140@gmail.com>
2026-07-06 20:17:59 -0300

    feat: Remoção de arrays desnecessários, otimização do código para melhor leitura. Muitos detalhes ainda para correção futuras.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,82 +1,99 @@
 function myPage () {
-  const form = document.querySelector('.forms');
-  const result = document.querySelector('.comand');
-  const taskUser = document.querySelector('.taskPen');
-  const exeTask = document.querySelector('.taskExe');
+  const form = document.querySelector('.forms'); // ainda sem função
+  const result = document.querySelector('.comand'); //Botão de Criar tarefa
+  const taskUserPen = document.querySelector('.taskPen'); //Primeira etapa da tarefa: 'Pendente'
+  const exeTask = document.querySelector('.taskExe'); // Segunda etapa da tarefa: 'Executando/Em andamento'
+  const taskComplet = document.querySelector('.concTask'); // Última etapa da tarefa: 'Concluída'
   
-  const tasksPen = [];
-  const tasksExe = [];
-  const tasksConc = [];
-  const tasksInc = [];
+  const userTaskList = []; //Array para salvar as tarefas
   
-  
-  function moveTask(div) {
+  //Função para mover as tarefas entre 'Em andamento' e  'concluídas'
+  function moveTask(div, btn) {
     exeTask.appendChild(div);
+    btn.setAttribute('title', 'Concluir Tarefa');
+
+    btn.textContent = 'Concluir';
+
+    // Click para 'Tarefas Concluídas' e exclusão de botão de avançar
+    btn.addEventListener('click', () => {
+      taskComplet.appendChild(div);
+
+      btn.parentNode.removeChild(btn); //Remove o botão após a tarefa ser concluída.
+    })
   }
   
-  
+  //
   function waitEvent(event) {
     event.preventDefault();
-    const tarefa = document.getElementById("tarefas-usuario").value;    
+    const task = document.getElementById("tarefas-usuario").value; //Pega a tarefa do usuário informada no input
     
-    function pushTaskExe() {
-      tasksExe.push({
-        tarefaUsuario: tarefa,
-      })
-  }
-
-    if(tarefa === ''){
+    // MELHORAR ESSE TRECHO
+    // Função para não permiti input vazio ser enviado
+    if(task === ''){
       alert("Você não adicionou nenhuma tarefa.");
       return;
     }
     
-    tasksPen.push({
-      tarefaUsuario: tarefa,
+    // Tarefas adicionadas ao Array
+    userTaskList.push({
+      tarefaUsuario: task,
     });
 
 
-    const divTasks = document.createElement('div');
-    const p = document.createElement('p');
+    const divTasks = document.createElement('div'); // Criação de uma Div para receber a tarefa em 'Pendentes'.
+    const p = document.createElement('p'); // Criação de um paragrafo para receber a tarefa.
     
-    divTasks.classList.add('taskAddUser');
-    p.classList.add('textTask');
+    divTasks.classList.add('taskAddUser'); // Adicionando classe à div
+    p.classList.add('textTask'); // Adicionando classe ao paragráfo
     
     
-    divTasks.appendChild(p);
-    createBtns(divTasks);
-    pushTaskExe;
+    taskUserPen.appendChild(divTasks); // Unindo div da tarefa à div de pendentes. 
+    divTasks.appendChild(p); // Unindo o paragráfo à div de tarefa.
+    createBtns(divTasks); // Executando a função de criar botões na tarefa
     
-    taskUser.appendChild(divTasks);
     
-    console.log(tasksPen);
-    console.log(tasksExe);
-    p.textContent += tarefa;
+    console.log(userTaskList); //Exibi o array 
+    p.textContent += task; // Mostra na página as tarefas criadas
     
   }
   
-  result.addEventListener('click', waitEvent);
+  result.addEventListener('click', waitEvent); //Executa a função principal
 
+    // Função que cria botões para manipular a tarefa dentro da página
     function createBtns(div){
-      const btnSetTask = document.createElement('button');
-    const btnDel = document.createElement('button');
+    const btnSetTask = document.createElement('button'); //Cria botão de avançar a tarefa
+    const btnDel = document.createElement('button'); // Cria botão de excluir a tarefa
     
-    btnSetTask.classList.add('btnSetTask');
+    // Adicionando classe aos botões
+    btnSetTask.classList.add('btnSetTask'); 
     btnDel.classList.add('DelTask');
+    
+    // Adicionando titulos aos botões
+    btnSetTask.setAttribute('title', 'Iniciar Tarefa');
+    btnDel.title = 'Deletar Tarefa';
 
+    // Mensagem nos botões
     btnSetTask.textContent = 'iniciar'
     btnDel.textContent = 'X'
-    btnDel.title = 'Deletar Tarefa'
 
+    // Utilizando parâmetro para alocar os botões à div da tarefa
     div.appendChild(btnSetTask);
     div.appendChild(btnDel);
-    
-    btnSetTask.addEventListener('click', () => {
-      moveTask(div);
+
+    // Click de Exclusão da tarefa
+    btnDel.addEventListener('click', (e) => {
+      const el = e.target;
+
+      if(el.classList.contains('DelTask')) {
+        el.parentElement.remove();
+      }
     });
     
+    // Click de movimentação da tarefa para outras etapas
+    btnSetTask.addEventListener('click', () => {
+      moveTask(div, btnSetTask);
+    });
   }
-
-
 }
 
 myPage();
